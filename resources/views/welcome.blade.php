ea818c27e39c57ac931e89edc63ddc516124660c
jesse <719785753@qq.com>
2017-01-09 00:10:19 +0800

    text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,15 +75,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   欢迎大黄同学来检查
+                  Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+              感谢laravist社区，及JellyBool ！！
                 </div>
             </div>
         </div>
